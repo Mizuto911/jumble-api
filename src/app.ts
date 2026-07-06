acc928ea4695a59d5817cb64530eaad49d48fad1
Masato Mizunuma <masatomizunuma911@gmail.com>
2026-07-06 17:34:35 +0800

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json("Health Check Complete");
+});
+
+app.listen(3000, () => {
+  console.log(`Jumble API is listening to port ${port}...`);
+});
