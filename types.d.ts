a735a25d3b5979ad86452eb6525ca3d2f51431d1
Masato Mizunuma <masatomizunuma911@gmail.com>
2026-07-07 13:44:36 +0800

    Added Types for Schema

--- types.d.ts
+++ types.d.ts
@@ -4,3 +4,28 @@ type delayQuery = {
   value?: number | undefined;
   units?: Units | undefined;
 };
+
+type SchemaElementType = "string" | "number" | "boolean" | "date";
+
+type SchemaElement =
+  | {
+      type: SchemaElementType | SchemaElementType[] | "*";
+      sampleResponse?: any | any[] | undefined;
+      contentType?:
+        | "longText"
+        | "name"
+        | "email"
+        | "phone"
+        | "age"
+        | "url"
+        | "imageUrl"
+        | "address"
+        | "price"
+        | "currency"
+        | "uuid";
+    }
+  | SchemaElementType;
+
+type Schema = {
+  [key: string]: SchemaElement;
+};
