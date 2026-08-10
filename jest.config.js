d4480da22d5ffb521bec1933678585a475dc447a
Masato Mizunuma <masatomizunuma911@gmail.com>
2026-08-10 17:12:49 +0800

    Configuration of jest for TypeScript Support

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
-module.exports = {
-  preset: "ts-jest",
-  testMatch: ["src/test/*.test.ts"],
+export default {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  roots: ["./src/test"],
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
+  },
+  // Maps explicit ESM imports (.js) back to your original source files (.ts)
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  testRegex: "(/__test__/.*|(\\.|/)(test|spec))\\.ts?$",
+  moduleFileExtensions: ["ts", "js", "json", "node"],
 };
