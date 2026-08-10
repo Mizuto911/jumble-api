ba68e9750cbcd82670cdd52fca5d22e2bd304fd3
Masato Mizunuma <masatomizunuma911@gmail.com>
2026-08-10 20:10:01 +0800

    Implemented Status Endpoint Unit Testing

--- src/test/status.test.ts
+++ src/test/status.test.ts
@@ -3,34 +3,59 @@ import testSchemas from "./testSchemas.js";
 import request from "supertest";
 import invalidSchema from "./invalidSchema.js";
 import { describe, it, expect } from "vitest";
+import { STATUS_CODES } from "node:http";
 
 const app = createApp(3030, testSchemas);
+const statusCodes = Object.keys(STATUS_CODES);
+const specialStatusCodes = [100, 101, 102, 103, 204, 205, 304];
 
-describe("GET /status/:status", () => {
+const isEmptyOrFalsey = (val: any) => {
+  if (!val) return true;
+  return Object.keys(val).length === 0 && val.constructor === Object;
+};
+
+describe("GET /api/status/:status", () => {
   describe("given a certain status code as params", () => {
-    it("should respond with a 200 status code", async () => {
+    it("should respond with a 200 status code and return default schema data", async () => {
       const response = await request(app).get("/api/status/200");
       expect(response.statusCode).toBe(200);
+      expect(response.body.success).toBe(true);
+      expect(response.body).toHaveProperty("data");
     });
 
-    it("should respond with a 201 status code", async () => {
+    it("should respond with a 201 status code and return default schema data", async () => {
       const response = await request(app).get("/api/status/201");
       expect(response.statusCode).toBe(201);
+      expect(response.body.success).toBe(true);
+      expect(response.body).toHaveProperty("data");
     });
 
-    it("should respond with a 400 status code", async () => {
+    it("should respond with a 400 status code and return default schema data", async () => {
       const response = await request(app).get("/api/status/400");
       expect(response.statusCode).toBe(400);
+      expect(response.body.success).toBe(true);
+      expect(response.body).toHaveProperty("data");
     });
 
-    it("should respond with a 404 status code", async () => {
+    it("should respond with a 404 status code and return default schema data", async () => {
       const response = await request(app).get("/api/status/404");
       expect(response.statusCode).toBe(404);
+      expect(response.body.success).toBe(true);
+      expect(response.body).toHaveProperty("data");
     });
 
-    it("should respond with a 500 status code", async () => {
+    it("should respond with a 500 status code and return default schema data", async () => {
       const response = await request(app).get("/api/status/500");
       expect(response.statusCode).toBe(500);
+      expect(response.body.success).toBe(true);
+      expect(response.body).toHaveProperty("data");
+    });
+
+    it("should respond with a 400 status code and return invalid status code error", async () => {
+      const response = await request(app).get("/api/status/550");
+      expect(response.statusCode).toBe(400);
+      expect(response.body.success).toBe(false);
+      expect(response.body).toHaveProperty("msg");
     });
   });
 
@@ -53,7 +78,7 @@ describe("GET /status/:status", () => {
       expect(response.body).toHaveProperty("data");
     });
 
-    it("should respond with a 404 status code and return not found message", async () => {
+    it("should respond with a 404 status code and return schema not found error", async () => {
       const response = await request(app).get(
         "/api/status/404?schemaID=schemaThatDoesntExist",
       );
@@ -62,9 +87,18 @@ describe("GET /status/:status", () => {
       expect(response.body).toHaveProperty("msg");
     });
 
-    it("should responsd with a 400 status code and return invalid range error message", async () => {
+    it("should respond with a 400 status code and return invalid range error message", async () => {
       const response = await request(app).get(
         "/api/status/500?schemaID=invalidRange",
+      );
+      expect(response.statusCode).toBe(400);
+      expect(response.body.success).toBe(false);
+      expect(response.body).toHaveProperty("msg");
+    });
+
+    it("should respond with a 400 status code and return invalid status code error", async () => {
+      const response = await request(app).get(
+        "/api/status/550?schemaID=invalidRange",
       );
       expect(response.statusCode).toBe(400);
       expect(response.body.success).toBe(false);
@@ -73,9 +107,9 @@ describe("GET /status/:status", () => {
   });
 });
 
-describe("POST /status/:status", () => {
+describe("POST /api/status/:status", () => {
   describe("given a certain status code as params and schema as body", () => {
-    it("should response with 200 status code and return generated data", async () => {
+    it("should respond with 200 status code and return generated data", async () => {
       const response = await request(app)
         .post("/api/status/200")
         .send(testSchemas.testSchema)
@@ -85,7 +119,7 @@ describe("POST /status/:status", () => {
       expect(response.body).toHaveProperty("data");
     });
 
-    it("should response with 400 status code and return generated data", async () => {
+    it("should respond with 400 status code and return generated data", async () => {
       const response = await request(app)
         .post("/api/status/400")
         .send(testSchemas.testSchema)
@@ -95,7 +129,7 @@ describe("POST /status/:status", () => {
       expect(response.body).toHaveProperty("data");
     });
 
-    it("should response with 400 status code and return invalid schema error", async () => {
+    it("should respond with 400 status code and return invalid schema error", async () => {
       const response = await request(app)
         .post("/api/status/200")
         .send(invalidSchema)
@@ -105,9 +139,124 @@ describe("POST /status/:status", () => {
       expect(response.body).toHaveProperty("msg");
     });
 
-    it("should response with 400 status code and return invalid range error", async () => {
+    it("should respond with 400 status code and return invalid range error", async () => {
       const response = await request(app)
         .post("/api/status/200")
+        .send(testSchemas.invalidRange)
+        .set("Accept", "application/json");
+      expect(response.statusCode).toBe(400);
+      expect(response.body.success).toBe(false);
+      expect(response.body).toHaveProperty("msg");
+    });
+
+    it("should respond with a 400 status code and return invalid status code error", async () => {
+      const response = await request(app)
+        .post("/api/status/550")
+        .send(testSchemas.userAdmin)
+        .set("Accept", "application/json");
+      expect(response.statusCode).toBe(400);
+      expect(response.body.success).toBe(false);
+      expect(response.body).toHaveProperty("msg");
+    });
+  });
+
+  describe("given a certain status code as params and no schema passed in body", () => {
+    it("should respond with 400 status code and return invalid schema error", async () => {
+      const response = await request(app).post("/api/status/200");
+      expect(response.statusCode).toBe(400);
+      expect(response.body.success).toBe(false);
+      expect(response.body).toHaveProperty("msg");
+    });
+
+    it("should respond with 400 status code and return invalid status code error", async () => {
+      const response = await request(app).post("/api/status/600");
+      expect(response.statusCode).toBe(400);
+      expect(response.body.success).toBe(false);
+      expect(response.body).toHaveProperty("msg");
+    });
+  });
+});
+
+describe("GET /api/status/random", () => {
+  describe("given no schemaID as query", () => {
+    it("should respond with a random status code and return default schema data", async () => {
+      const response = await request(app).get("/api/status/random");
+      expect(statusCodes).toContain(String(response.statusCode));
+
+      if (specialStatusCodes.includes(response.statusCode)) {
+        expect(response.body).toSatisfy(isEmptyOrFalsey);
+      } else {
+        expect(response.body.success).toBe(true);
+        expect(response.body).toHaveProperty("data");
+      }
+    });
+  });
+
+  describe("given a certain schemaID as query", () => {
+    it("should respond with a random status code and return generated data", async () => {
+      const response = await request(app).get(
+        "/api/status/random?schemaID=userAdmin",
+      );
+      expect(statusCodes).toContain(String(response.statusCode));
+
+      if (specialStatusCodes.includes(response.statusCode)) {
+        expect(response.body).toSatisfy(isEmptyOrFalsey);
+      } else {
+        expect(response.body.success).toBe(true);
+        expect(response.body).toHaveProperty("data");
+      }
+    });
+
+    it("should respond 404 status code and return schema not found error", async () => {
+      const response = await request(app).get(
+        "/api/status/random?schemaID=userAdmdw",
+      );
+      expect(response.statusCode).toBe(404);
+      expect(response.body.success).toBe(false);
+      expect(response.body).toHaveProperty("msg");
+    });
+
+    it("should respond 400 status code and return invalid range error", async () => {
+      const response = await request(app).get(
+        "/api/status/random?schemaID=invalidRange",
+      );
+      expect(response.statusCode).toBe(400);
+      expect(response.body.success).toBe(false);
+      expect(response.body).toHaveProperty("msg");
+    });
+  });
+});
+
+describe("POST /api/status/random", () => {
+  describe("given a schema passed in body", () => {
+    it("should respond with a random status code and return generated data", async () => {
+      const response = await request(app)
+        .post("/api/status/random")
+        .send(testSchemas.userAdmin)
+        .set("Accept", "application/json");
+      expect(statusCodes).toContain(String(response.statusCode));
+
+      if (specialStatusCodes.includes(response.statusCode)) {
+        expect(response.body).toSatisfy(isEmptyOrFalsey);
+      } else {
+        expect(response.body.success).toBe(true);
+        expect(response.body).toHaveProperty("data");
+      }
+    });
+
+    it("should respond 400 status code and return invalid schema error", async () => {
+      const response = await request(app)
+        .post("/api/status/random")
+        .send(invalidSchema)
+        .set("Accept", "application/json");
+      expect(response.statusCode).toBe(400);
+      expect(response.body.success).toBe(false);
+      expect(response.body).toHaveProperty("msg");
+    });
+
+    it("should respond 400 status code and return invalid range error", async () => {
+      const response = await request(app)
+        .post("/api/status/random")
         .send(testSchemas.invalidRange)
         .set("Accept", "application/json");
       expect(response.statusCode).toBe(400);
