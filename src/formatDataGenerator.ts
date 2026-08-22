6b2ce82bfe98f593b44d1fe2a101fb7c8f0f3fc6
Masato Mizunuma <masatomizunuma911@gmail.com>
2026-08-22 21:38:44 +0800

    refactor: simplify object look up to free up space complexity

--- src/formatDataGenerator.ts
+++ src/formatDataGenerator.ts
@@ -1,134 +1,82 @@
 import { faker } from "@faker-js/faker";
-import { InvalidRangeError } from "./error.js";
+import { InvalidRangeError, InvalidDateStringError } from "./error.js";
 import { getRandomTrueOrFalse } from "./utilities.js";
 
-const string = (min?: number | string | null, max?: number | string | null) => {
-  const minExist = min != null;
-  const maxExist = max != null;
+type FormatTypes = PrimaryTypes | MockTypes;
+type Generator = (
+  min?: number | string | null,
+  max?: number | string | null,
+) => any;
 
-  if (
-    (minExist && typeof min !== "number") ||
-    (maxExist && typeof max !== "number")
-  )
-    throw new InvalidRangeError(
-      "The 'min' and 'max' property must be of type 'number' for string format generation.",
-    );
-  return faker.lorem.words({
-    min: min ?? 1,
-    max: maxExist ? max : minExist ? min + 10 : 10,
-  });
-};
-
-const number = (min?: number | string | null, max?: number | string | null) => {
+function requireNumericRange(
+  min?: number | string | null,
+  max?: number | string | null,
+) {
   if (
     (min != null && typeof min !== "number") ||
     (max != null && typeof max !== "number")
   )
     throw new InvalidRangeError(
-      "The 'min' and 'max' property must be of type 'number' for number format generation.",
+      "The 'min' and 'max' property must be of type 'number' for string format generation.",
     );
-  return faker.number.int({ min: min ?? 0, max: max ?? 99999999999999 });
-};
 
-const boolean = () => {
-  return getRandomTrueOrFalse();
-};
+  return { rMin: min, rMax: max };
+}
 
-const date = (min?: number | string | null, max?: number | string | null) => {
-  let from: string | number;
-  let to: string | Date | number;
+function parseToDate(value?: string | number | null) {
+  if (value == null) return undefined;
+  const timestamp =
+    typeof value === "number" ? value : new Date(value).getTime();
+  if (isNaN(timestamp)) throw new InvalidDateStringError();
 
-  if (min != null && typeof min === "string") from = min;
-  else from = new Date().getTime() - 157788000000; // 5 years before now
-
-  if (max != null && typeof max === "string") to = max;
-  else to = new Date().getTime() + 157788000000; // 5 years after now
-  return faker.date.between({ from, to });
-};
-
-const sex = () => {
-  return faker.person.sex();
-};
-
-const fullname = () => {
-  return faker.person.fullName();
-};
-
-const firstname = () => {
-  return faker.person.firstName();
-};
-
-const lastname = () => {
-  return faker.person.lastName();
-};
-
-const email = () => {
-  return faker.internet.email();
-};
-
-const phone = () => {
-  return `09${faker.string.numeric(9)}`;
-};
-
-const url = () => {
-  return faker.internet.url();
-};
-
-const imageUrl = () => {
-  return faker.image.urlPicsumPhotos();
-};
-
-const avatarUrl = () => {
-  return faker.image.avatar();
-};
-
-const portrait = () => {
-  return faker.image.personPortrait();
-};
-
-const countryCode = () => {
-  return faker.location.countryCode();
-};
-
-const address = () => {
-  return `${faker.location.streetAddress()}, ${faker.location.state()}, ${faker.location.country()}`;
-};
-
-const color = () => {
-  return faker.color.rgb();
-};
-
-const zipcode = () => {
-  return faker.location.zipCode();
-};
-
-const currency = () => {
-  return faker.finance.currencyCode();
-};
-
-const uuid = () => {
-  return faker.string.uuid();
-};
+  return timestamp;
+}
 
 export default {
-  string,
-  number,
-  boolean,
-  date,
-  sex,
-  fullname,
-  firstname,
-  lastname,
-  email,
-  phone,
-  url,
-  imageUrl,
-  avatarUrl,
-  portrait,
-  countryCode,
-  address,
-  color,
-  zipcode,
-  currency,
-  uuid,
-};
+  string: (min?: number | string | null, max?: number | string | null) => {
+    const { rMin, rMax } = requireNumericRange(min, max);
+    return faker.lorem.words({
+      min: rMin ?? 1,
+      max: rMax != null ? rMax : rMin != null ? rMin + 10 : 10,
+    });
+  },
+  number: (min?: number | string | null, max?: number | string | null) => {
+    const { rMin, rMax } = requireNumericRange(min, max);
+    return faker.number.int({ min: rMin ?? 0, max: rMax ?? 99999999999999 });
+  },
+  boolean: () => getRandomTrueOrFalse(),
+  date: (min?: number | string | null, max?: number | string | null) => {
+    let from: number;
+    let to: number;
+
+    const minDate = parseToDate(min);
+    const maxDate = parseToDate(max);
+
+    from = minDate != null ? minDate : new Date().getTime() - 157788000000;
+    to =
+      maxDate != null
+        ? maxDate
+        : minDate != null
+          ? minDate + 157788000000 * 2
+          : new Date().getTime() + 157788000000;
+
+    return faker.date.between({ from, to });
+  },
+  sex: () => faker.person.sex(),
+  fullname: () => faker.person.fullName(),
+  firstname: () => faker.person.firstName(),
+  lastname: () => faker.person.lastName(),
+  email: () => faker.internet.email(),
+  phone: () => `09${faker.string.numeric(9)}`,
+  url: () => faker.internet.url(),
+  imageUrl: () => faker.image.urlPicsumPhotos(),
+  avatarUrl: () => faker.image.avatar(),
+  portrait: () => faker.image.personPortrait(),
+  countryCode: () => faker.location.countryCode(),
+  address: () =>
+    `${faker.location.streetAddress()}, ${faker.location.state()}, ${faker.location.country()}`,
+  color: () => faker.color.rgb(),
+  zipcode: () => faker.location.zipCode(),
+  currency: () => faker.finance.currencyCode(),
+  uuid: () => faker.string.uuid(),
+} satisfies Record<FormatTypes, Generator>;
